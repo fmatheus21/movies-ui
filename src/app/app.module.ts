65518d6628f39756405e8206391fd7fe843fc082
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-02-20 05:57:36 -0300

    Implementação de module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './pages/template/core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    FormsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
