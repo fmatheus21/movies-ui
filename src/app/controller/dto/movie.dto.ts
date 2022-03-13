04bd406b3c6f33689a28fad11e81c2e28256c74b
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-03-13 00:54:34 -0300

    Implementação de dto.

---
+++ src/app/controller/dto/movie.dto.ts
@@ -0,0 +1,13 @@
+export class MovieDto {
+  id!: number;
+  title!: string;
+  year!: number;
+  rating!: number;
+  urlTrailer!: string;
+  createdBy!: string;
+  createdAt!: string;
+  updatedBy!: string;
+  updatedAt!: string;
+  image!: string;
+  colorRating!: string;
+}
